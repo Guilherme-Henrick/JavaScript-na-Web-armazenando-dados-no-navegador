3abfe8a7fba2cfed608b346fd8ed7ba600724597
Guilherme-Henrick <henrick.guilherme@gmail.com>
2022-11-16 21:58:10 -0300

    Conferindo e atualizando itens

--- Alura-mochila-de-viagem/js/main.js
+++ Alura-mochila-de-viagem/js/main.js
@@ -1,38 +1,59 @@
-const form = document.querySelector("#novoItem");
-const lista = document.getElementById('lista');
-const itens = JSON.parse(localStorage.getItem("itens") || []);
+const form = document.getElementById("novoItem") ;
+const lista = document.getElementById("lista");
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
-form.addEventListener("submit", (evento) => {
+itens.forEach( (elemento) => {    
+    criaElemento(elemento);
+} )
+
+form.addEventListener("submit", (evento) => {  
     evento.preventDefault();
 
-    const nome = evento.target.elements[0];
-    const quantidade = evento.target.elements[1];
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
+
+    const existe = itens.find(elemento => elemento.nome === nome.value);
+    console.log(existe);
 
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
+        }
+
+    if(existe) {
+        itemAtual.id = existe.id;
+
+        atualizaElemento(itemAtual);
+
+    } else {
+        itemAtual.id = itens.length;
+
+        criaElemento(itemAtual);
+
+        itens.push(itemAtual);
     }
-
-    criaElemento(itemAtual);
-
-    itens.push(itemAtual);
 
     localStorage.setItem("itens", JSON.stringify(itens));
 
     nome.value = "";
     quantidade.value = "";
-});
+})
 
-function criaElemento(item) {
-/* <li class="item"><strong>7</strong>Camisas</li> */
+function criaElemento(item) {  
     const novoItem = document.createElement('li');
-    const numeroItem = document.createElement('strong');
-
     novoItem.classList.add("item");
 
+    const numeroItem = document.createElement('strong');
     numeroItem.innerHTML = item.quantidade;
+    numeroItem.dataset.id = item.id;
     novoItem.appendChild(numeroItem);
+
     novoItem.innerHTML += item.nome;
 
     lista.appendChild(novoItem);
 };
+
+function atualizaElemento(item){
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
+
+}
